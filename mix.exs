7bef061152adfe26b3eea187ee2a65b99c35b3ff
Justin Edwards <justin@igrowbeards.com>
2019-07-22 13:51:15 -0500

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DeployMe.Mixfile do
   def project do
     [
       app: :deploy_me,
-      version: "0.1.14",
+      version: "0.1.16",
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
